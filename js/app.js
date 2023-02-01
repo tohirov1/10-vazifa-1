ea152ffc1cf3fb405657c77b18e7724160b3e6d0
Your Name <you@example.com>
2023-02-01 18:44:49 +0500

    new add js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 let elInput = document.querySelector('.list-input')
 let elBtn = document.querySelector('.click-button')
 let elList = document.querySelector('.list')
-
+let elInput1 = document.querySelector('.list-input1')
 
 elBtn.addEventListener('click', function () {
    let elItem = document.createElement("li")
-   elItem.textContent = elInput.value
+   elItem.textContent = `${elInput1.value} : ${elInput.value}`
    elList.appendChild(elItem)
    elInput.value = ""
+   elInput1.value = ""
 })
