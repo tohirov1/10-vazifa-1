5c8f56dbb1f6ff9368d481e64ecf39c3e9d6f300
Your Name <you@example.com>
2023-02-01 15:50:41 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let elInput = document.querySelector('.list-input')
+let elBtn = document.querySelector('.click-button')
+let elList = document.querySelector('.list')
+
+
+elBtn.addEventListener('click', function () {
+   let elItem = document.createElement("li")
+   elItem.textContent = elInput.value
+   elList.appendChild(elItem)
+})
